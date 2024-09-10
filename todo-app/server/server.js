6a1b5fd83155077192d583945fe2458750db1c92
mbarto202 <michael.d.barto@gmail.com>
2024-09-09 23:24:54 -0400

    conected to db

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -3,14 +3,14 @@ const {Client} = require("pg");
 const app = express();
 const client = new Client({
     user: "postgres",
-    //password: "",
+    password: "G00dbye202!2",
     host: "localhost",
     port: 5432,
     database: "todo_app"
 })
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5174");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     res.header("Access-Control-Allow-Credentials", true);
     res.header(
         "Access-Control-Allow-Headers",
@@ -35,7 +35,7 @@ async function connect(){
 }
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/todos', async (req, res) => {
+app.get("/todos", async (req, res) => {
   try{
     const results = await client.query("select * from todos");
     res.json(results.rows)
@@ -46,5 +46,16 @@ app.get('/todos', async (req, res) => {
 
   res.json([])
 })
+
+app.post("/todos", async (req, res) => {
+    try{
+      res.json({status: "success"})
+    } catch (e) {
+      console.log("There was an error");
+      res.send("There was an error")
+    }
+  
+    res.json([])
+  })
 
 app.listen(5001, () => console.log("listening on port 5001...."));
--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState([]);
 
   useEffect(() => {
     fetchTodos();
@@ -16,8 +17,24 @@ function App() {
     setTodos(data)
   }
 
+  const createTodo = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5001/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({todo})
+    });
+    setTodo("")
+    fetchTodos();
+  }
+
   return <div className="App">
     <h1>Todos</h1>
+    <form action="" onSubmit={createTodo}>
+      <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} />
+    </form>
     {todos.map((t) => (
     <div key={t.id}>{t.title}</div>))}</div>
 
